529c7f0fef699321e69f32ad059cda517d1b5ead
Lpoloc <lpoloczek@outlook.com>
2022-01-20 17:00:45 +0000

    Initial commit. Created basic file structure.

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+var { engine } = require('express-handlebars');
+
+const app = express();
+
+// App Configuration
+
+app.engine('.hbs', engine({ extname : '.hbs', defaultLayout : 'main'}));
+
+app.set('port', process.env.PORT || 8000);
+app.set('view engine', '.hbs');
+
+// App middleware
+
+app.use(express.static(__dirname + '/public'));
+
+// App Routes
+
+app.get('/', function(req, res){
+    var options = { title : 'Homepage'};
+    res.render('home', options);
+});
+
+// custom 404 page
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Server running on http://127.0.0.1:' + app.get('port') + '; press CTRL + C to terminate');
+});
