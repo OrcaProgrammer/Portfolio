45321554f76b5765832a1a0a62f2f5c883d43a57
Lpoloc <lpoloczek@outlook.com>
2022-03-13 22:43:44 +0000

    Added uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,28 @@
 module.exports = function(grunt) {
-    
+
+    [
+        'grunt-browserify',
+        'grunt-contrib-uglify'
+    ].forEach(task => {
+        grunt.loadNpmTasks(task);
+    })
+
     grunt.initConfig({
         browserify: {
             client: {
                 src: ['client/*.js','client/**/*.js'],
                 dest: 'public/scripts/main.js'
             }
+        },
+        uglify: {
+            all: {
+                files: {
+                    'public/scripts/main.min.js': 'public/scripts/main.js'
+                }
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-browserify');
     grunt.registerTask('start', 'browserify');
+    grunt.registerTask('clean', 'uglify');
 }
